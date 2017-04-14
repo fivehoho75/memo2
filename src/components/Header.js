9a4d7d519be88bc7d9125ca3c4762270818cc84a
byung rae kim <byung rae kim>
2017-04-14 16:36:54 +0900

    step02(reducer 전까지)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
- 
+import { Link } from 'react-router';
+
 class Header extends React.Component {
     render() {
         const loginButton = (
             <li>
-                <a>
+                <Link to="/login">
                     <i className="material-icons">vpn_key</i>
-                </a>
+                </Link>
             </li>
         );
  
@@ -21,7 +22,7 @@ class Header extends React.Component {
         return (
             <nav>
                 <div className="nav-wrapper blue darken-1">
-                    <a className="brand-logo center">MEMOPAD</a>
+                    <Link to="/" className="brand-logo center">MEMOPAD</Link>
  
                     <ul>
                         <li><a><i className="material-icons">search</i></a></li>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Header } from 'components';
+import { Header} from 'components';
  
 class App extends React.Component {
     render(){
+        /* Check whether current route is login or register using regex */
+        let re = /(login|register)/;
+        let isAuth = re.test(this.props.location.pathname);
  
         return (
             <div>
-                <Header/>
+                {isAuth ? undefined : <Header/>}
                 { this.props.children }
             </div>
         );
